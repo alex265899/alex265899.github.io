2eef13613ab1bafdad8e77c8ccf6a75a536f9fc7
Alexander Chapman <ouralexander@gmail.com>
2016-05-27 17:48:25 -0400

    Added the JavaScript file; Still need to get the alerts to work

--- loginPage/loginScript.js
+++ loginPage/loginScript.js
@@ -3,7 +3,7 @@
  */
 var username = 'alex265899';
 var password = 'brent16';
-//var loginAtempts = 0;
+//var loginAttempts = 0;
 function loginForm(){
     var  usernameInput = document.forms['Login']['userInput'].value;
     var passwordInput = document.forms['Login']['passInput'].value;
@@ -12,7 +12,7 @@ function loginForm(){
         window.alert("Correct login. Welcome!");
         return true;
     } else {
-        window.alert("Login unsuccessful. Incorrect username or password!")
+        window.alert("Login unsuccessful. Incorrect username or password!");
         return false;
     }
 
