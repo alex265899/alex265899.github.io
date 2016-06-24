df9c7d333e6e94cd1894ca2f5ce4cae1c6ff2d67
Alexander Chapman <ouralexander@gmail.com>
2016-06-24 09:56:34 -0400

    Added a copy of the original xml file. Also working on a python file toadd xml tags for me.

---
+++ Webpage/mainPage/xmlPatternMaker.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+nameInput = raw_input("Name: ")
+fileInput = raw_input("File Location (do not add .jpg), ../../EmbroideryPatterns/")
+catogoryInput = raw_input("Category: ")
+sizeInput = raw_input("Size: ")
+stitchInput = raw_input("Number of stitches: ")
+colorInput = raw_input("Number of colors: ")
+
+xmlFile = open('inventoryCopy.xml', mode='a+')
+xmlFile.writelines("""
+    <pattern>
+        <name>{}</name>
+        <image>../../EmbroideryPatternScans/{}.jpg)</image>
+        <category>{}</category>
+        <size>{}</size>
+        <stitches>{}</stitches>
+        <colors>{}</colors>
+    </pattern>""".format(nameInput,fileInput,catogoryInput,sizeInput,stitchInput,colorInput))
+xmlFile.close()
